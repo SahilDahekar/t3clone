6c77b48b9c637f40d6ec387570061d1df72b6ea4
SahilDahekar <dahekar30sahil@gmail.com>
2025-06-11 23:19:54 +0530

    cleaned api/chat/route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,34 +1,34 @@
-import { NextResponse } from "next/server"
 import { google } from '@ai-sdk/google';
-import { streamText } from 'ai';
-import { text } from "stream/consumers";
-import { type LanguageModel } from "ai";
+import { streamText , type LanguageModel } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 const getModel = (selectedModel: string): LanguageModel => {
-  switch (selectedModel) {
-    case 'gemini-2.0-flash-lite':
-      return google('models/gemini-2.0-flash-lite');
-    case 'gemini-2.0-pro':
-      return google('models/gemini-2.0-pro');   
-    default:
-        return google('models/gemini-2.0-flash-lite');   
-  }
+  // switch (selectedModel) {
+  //   case 'gemini-2.0-flash-lite':
+  //     return google('models/gemini-2.0-flash-lite');
+  //   case 'gemini-2.0-pro':
+  //     return google('models/gemini-2.0-pro');   
+  //   default:
+  //       return google('models/gemini-2.0-flash-lite');   
+  // }
+  return google('models/gemini-2.0-flash-lite');
 }        
   
 export async function POST(req: Request) {
   console.log('Received request for chat route'+ req);
   const { messages , selectedModel} = await req.json();
-  const model:LanguageModel = getModel(selectedModel);
+  const model : LanguageModel = getModel(selectedModel);
   const result = streamText({
-    model: google('models/gemini-2.0-flash-lite'),
+    model: model,
     prompt: messages,
   });
+
 //   for await (const textPart of textStream) {
 //       console.log(textPart);
 // }
+
   return result.toDataStreamResponse();
 }
 
